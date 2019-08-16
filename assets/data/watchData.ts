9c898b4fe6725d0eb8b4eb283ce6a8140d03f195
Hyunju Woo <44936075+DeveloperWoo@users.noreply.github.com>
2019-08-16 13:01:33 -0400

    Add files via upload

---
+++ assets/data/watchData.ts
@@ -0,0 +1,7 @@
+import { Watch } from '../../app/watch';
+
+export const WATCHDATA : Watch[] = [
+    {style: 'classic', color: 'black', material: 'metal'},
+    {style: 'fancy', color: 'white', material: 'silver'},
+    {style: 'elegant', color: 'pink gold', material: 'gold and diamond'}
+];
